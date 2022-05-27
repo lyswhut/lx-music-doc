f806eca10683d0c67d3d53bf07a1491415625e42
lyswhut <lyswhut@qq.com>
2022-05-28 00:22:43 +0800

    添加更新时间显示

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -48,6 +48,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl: 'https://github.com/lyswhut/lx-music-doc/tree/master/',
+          showLastUpdateTime: true,
         },
         blog: false,
         theme: {
