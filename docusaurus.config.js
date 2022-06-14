f4a9e34160dc2a764ad6ce669b18f627d3828388
lyswhut <lyswhut@qq.com>
2022-06-14 09:52:44 +0800

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -48,7 +48,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl: 'https://github.com/lyswhut/lx-music-doc/tree/master/',
-          showLastUpdateAuthor: true,
+          // showLastUpdateAuthor: true,
           showLastUpdateTime: true,
         },
         blog: false,
